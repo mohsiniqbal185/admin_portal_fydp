1c947736b0e4ea902a0ad2e4413d8c2f154339b4
Mehdi-Zafar <mehdizaffar0@gmail.com>
2023-08-06 05:28:16 +0500

    buttons added

--- frontend/src/pages/manageTokenTransactionsDetail/ManageTokenTransactionsDetail.jsx
+++ frontend/src/pages/manageTokenTransactionsDetail/ManageTokenTransactionsDetail.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import Container from '../../components/container/Container'
 import InfoFields from '../../components/infoFields/InfoFields'
+import './manageTokenTransactionsDetail.scss'
 
 const details = [{title:'Holder ID'},{title:'Holder Name'},{title:'Token Transaction ID'},{title:'Property Name'},{title:'Property ID'},{title:'Sender ID'},{title:'Number of Tokens'},{title:'Transaction Hash'},{title:'Payment Status'}]
 
 function FieldsForInfo(){
   return (
-  <InfoFields details={details}/>
+    <>
+      <InfoFields details={details}/>
+      <div className='receipt-link'>
+            View Payment Receipt
+      </div>
+      <div className='btn'>
+        <button>Verify</button>
+      </div>
+      <div className='btn'>
+        <button>Transfer Tokens</button>
+      </div>
+    </>
   )
 }
 
-const componentData = {title:'Token Payments',caption:'The token payments information is listed below.',component:<FieldsForInfo/>}
+const componentData = {title:'Transaction Details',caption:'The transaction details information is listed below.',component:<FieldsForInfo/>}
 
 function ManageTokenTransactionsDetail() {
   return (
--- frontend/src/pages/tokenPaymentsDetail/TokenPaymentsDetail.jsx
+++ frontend/src/pages/tokenPaymentsDetail/TokenPaymentsDetail.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import InfoFields from '../../components/infoFields/InfoFields'
 import Container from '../../components/container/Container'
+import './tokenPaymentsDetail.scss'
 
 const details = [{title:'Payment ID'},{title:'Payment Method'},{title:'Payment From'},{title:'DateTime'},{title:'Transaction ID'},{title:'Payment Status'}]
 
 function FieldsForInfo(){
   return (
-  <InfoFields details={details}/>
+    <>
+      <InfoFields details={details}/>
+      <div className='receipt-link'>
+        View Payment Receipt
+      </div>
+    </>
   )
 }
 
